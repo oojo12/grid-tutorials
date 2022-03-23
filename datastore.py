228cdd01f7f87403920e3332264d918cfc9f7f9d
oojo12 <47154698+oojo12@users.noreply.github.com>
2022-03-23 14:21:46 -0400

    Update datastore.py

--- datastore.py
+++ datastore.py
@@ -2,17 +2,9 @@ import os
 import time
 import argparse
 
-time.sleep(60)
-def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
-            
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--data_dir', type=str)
 args = parser.parse_args()
-list_files(args.data_dir)
+dir_list = os.listdir(args.data_dir)
+time.sleep(60)
+print(dir_list)
