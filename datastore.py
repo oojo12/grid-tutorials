24266c65a4fd72459ba81ae52dfdac1b62f9237c
oojo12 <47154698+oojo12@users.noreply.github.com>
2022-03-22 23:06:34 -0400

    Create datastore.py

---
+++ datastore.py
@@ -0,0 +1,17 @@
+import os
+import argparse
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+            
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser.add_argument('startpath', type=str)
+  args = parser.parse_args()
+  list_files(args.startpath)
